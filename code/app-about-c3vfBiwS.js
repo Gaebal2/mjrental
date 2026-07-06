82ef002f3f8793a8dadc32ea7d5db2049a4f204c
Gaebal2 <gaebal2017@gmail.com>
2026-07-07 00:14:38 +0900

    Updates

--- code/app-about-c3vfBiwS.js
+++ code/app-about-c3vfBiwS.js
@@ -1,4 +1,4 @@
-import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-Dwv4ApyX.js";const g=l`
+import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-BCje4kh1.js";const g=l`
   @media(min-width: 1000px) {
     sl-card {
       max-width: 70vw;
@@ -110,4 +110,4 @@ import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-Dwv4
         </sl-card>
       </main>
     `}};t.styles=[_,g];t=x([v("app-about")],t);export{t as AppAbout};
-//# sourceMappingURL=app-about-aGq94elI.js.map
+//# sourceMappingURL=app-about-c3vfBiwS.js.map
