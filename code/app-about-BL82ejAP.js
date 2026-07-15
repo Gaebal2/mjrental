5adb31cdc864f4f62f1952574761333cb95256b8
Clazyman <101004958+Gaebal2@users.noreply.github.com>
2026-07-15 03:50:46 +0000

    Updates

--- code/app-about-BL82ejAP.js
+++ code/app-about-BL82ejAP.js
@@ -1,4 +1,4 @@
-import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-Dy-ROwxf.js";const g=l`
+import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-Chg8PF8N.js";const g=l`
   @media(min-width: 1000px) {
     sl-card {
       max-width: 70vw;
@@ -110,4 +110,4 @@ import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-Dy-R
         </sl-card>
       </main>
     `}};t.styles=[_,g];t=x([v("app-about")],t);export{t as AppAbout};
-//# sourceMappingURL=app-about-DAc6QrnA.js.map
+//# sourceMappingURL=app-about-BL82ejAP.js.map
