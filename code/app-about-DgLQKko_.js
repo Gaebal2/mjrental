c87d2f90596dccad3131a0feb8ff76774f90c125
Clazyman <101004958+Gaebal2@users.noreply.github.com>
2026-07-15 00:53:09 +0000

    Updates

--- code/app-about-DgLQKko_.js
+++ code/app-about-DgLQKko_.js
@@ -1,4 +1,4 @@
-import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-r7vXpJ4s.js";const g=l`
+import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-BZ4M775Q.js";const g=l`
   @media(min-width: 1000px) {
     sl-card {
       max-width: 70vw;
@@ -110,4 +110,4 @@ import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-r7vX
         </sl-card>
       </main>
     `}};t.styles=[_,g];t=x([v("app-about")],t);export{t as AppAbout};
-//# sourceMappingURL=app-about-CF5beHWa.js.map
+//# sourceMappingURL=app-about-DgLQKko_.js.map
