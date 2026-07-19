94165ecd8d133489975857ff2077137b28d211c7
Clazyman <101004958+Gaebal2@users.noreply.github.com>
2026-07-19 23:25:26 +0000

    Updates

--- code/app-about-DwwSadX3.js
+++ code/app-about-DwwSadX3.js
@@ -1,4 +1,4 @@
-import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-fS4YIGIh.js";const g=l`
+import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-DiUANnl_.js";const g=l`
   @media(min-width: 1000px) {
     sl-card {
       max-width: 70vw;
@@ -110,4 +110,4 @@ import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-fS4Y
         </sl-card>
       </main>
     `}};t.styles=[_,g];t=x([v("app-about")],t);export{t as AppAbout};
-//# sourceMappingURL=app-about-DLyLiW3n.js.map
+//# sourceMappingURL=app-about-DwwSadX3.js.map
