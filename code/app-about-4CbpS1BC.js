c9454d8309bd739cec50d7a3e7e20c2d9e03c3fd
Clazyman <101004958+Gaebal2@users.noreply.github.com>
2026-07-19 22:11:54 +0000

    Updates

--- code/app-about-4CbpS1BC.js
+++ code/app-about-4CbpS1BC.js
@@ -1,4 +1,4 @@
-import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-BGriFNVD.js";const g=l`
+import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-BXfN4xK-.js";const g=l`
   @media(min-width: 1000px) {
     sl-card {
       max-width: 70vw;
@@ -110,4 +110,4 @@ import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-BGri
         </sl-card>
       </main>
     `}};t.styles=[_,g];t=x([v("app-about")],t);export{t as AppAbout};
-//# sourceMappingURL=app-about-BZk5tppS.js.map
+//# sourceMappingURL=app-about-4CbpS1BC.js.map
