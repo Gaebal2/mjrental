ef92558b29f23c179719b14c04bc804e5555472b
Clazyman <101004958+Gaebal2@users.noreply.github.com>
2026-07-19 23:06:21 +0000

    Updates

--- code/app-about-pA1Inve6.js
+++ code/app-about-pA1Inve6.js
@@ -1,4 +1,4 @@
-import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-CogOgWoJ.js";const g=l`
+import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-JkJJNic-.js";const g=l`
   @media(min-width: 1000px) {
     sl-card {
       max-width: 70vw;
@@ -110,4 +110,4 @@ import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-CogO
         </sl-card>
       </main>
     `}};t.styles=[_,g];t=x([v("app-about")],t);export{t as AppAbout};
-//# sourceMappingURL=app-about-Bm_UTi-P.js.map
+//# sourceMappingURL=app-about-pA1Inve6.js.map
