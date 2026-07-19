79347be758711a9b04f9f4ccd04f7c005886a6ec
Clazyman <101004958+Gaebal2@users.noreply.github.com>
2026-07-19 22:25:27 +0000

    Updates

--- code/app-about-D_QZ6ao6.js
+++ code/app-about-D_QZ6ao6.js
@@ -1,4 +1,4 @@
-import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-CRBpaSH8.js";const g=l`
+import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-BRlC1fck.js";const g=l`
   @media(min-width: 1000px) {
     sl-card {
       max-width: 70vw;
@@ -110,4 +110,4 @@ import{i as l,c as p,S as h,H as b,x as c,e as m,r as u,t as v}from"./index-CRBp
         </sl-card>
       </main>
     `}};t.styles=[_,g];t=x([v("app-about")],t);export{t as AppAbout};
-//# sourceMappingURL=app-about-qBwQk5-v.js.map
+//# sourceMappingURL=app-about-D_QZ6ao6.js.map
